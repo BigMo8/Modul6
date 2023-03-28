e75ea480bf3d29e0008174697ab2d15871f96213
Monika Birgiel Marszal <monikabirgiel@gmail.com>
2023-03-28 11:22:11 +0200

    Rearrange structure of objects

--- bazydanych.py
+++ bazydanych.py
@@ -1,22 +1,35 @@
-# ex_02_create_tables.py
-
 import sqlite3
 from sqlite3 import Error
+#import sqlalchemy
+#from sqlalchemy import create_engine
 
+db_file = "database.db"
+
+#NAWIĄZANIE POŁĄCZENIA Z BAZĄ 
 def create_connection(db_file):
-   """ create a database connection to the SQLite database
-       specified by db_file
-   :param db_file: database file
-   :return: Connection object or None
-   """
+   """ create a database connection to a SQLite database """
    conn = None
    try:
        conn = sqlite3.connect(db_file)
-       return conn
+       print(f"Connected to {db_file}, sqlite version: {sqlite3.version}")
    except Error as e:
        print(e)
-
+   #finally:
+   #    if conn:
+   #        conn.close()
    return conn
+
+#def create_connection_in_memory():
+#   """ create a database connection to a SQLite database """
+#   conn = None
+#   try:
+#       conn = sqlite3.connect(":memory:")
+#       print(f"Connected, sqlite version: {sqlite3.version}")
+#   except Error as e:
+#       print(e)
+#   finally:
+#       if conn:
+#           conn.close()
 
 def execute_sql(conn, sql):
    """ Execute sql
@@ -32,6 +45,93 @@ def execute_sql(conn, sql):
 # obiekt połączenia do bazy danych (conn) Oferuje on inny obiekt – cursor – przy pomocy którego możemy wykonywać zapytania
 # Drugi parametr to text, który jest kodem SQL
 
+#POZOSTAŁE FUNKCJE OPERACJI NA BAZIE - CRUD
+def add_project(conn, project):
+   """
+   Create a new project into the projects table
+   :param conn:
+   :param project:
+   :return: projekt id
+   """
+   sql = '''INSERT INTO projects(nazwa, start_date, end_date)
+             VALUES(?,?,?)'''
+   cur = conn.cursor()
+   cur.execute(sql, project)
+   conn.commit()
+   return cur.lastrowid
+
+def add_task(conn, task):
+   """
+   Create a new task into the tasks table
+   :param conn:
+   :param task:
+   :return: task id
+   """
+   sql = '''INSERT INTO tasks(projekt_id, nazwa, opis, status, start_date, end_date)
+             VALUES(?,?,?,?,?,?)'''
+   cur = conn.cursor()
+   cur.execute(sql, task)
+   conn.commit()  
+   return cur.lastrowid
+
+def select_task_by_status(conn, status):
+   """
+   Query tasks by priority
+   :param conn: the Connection object
+   :param status:
+   :return:
+   """
+   cur = conn.cursor()
+   cur.execute("SELECT * FROM tasks WHERE status=?", ("started", ))
+
+   rows = cur.fetchall()
+   return rows
+
+def select_all(conn, table):
+   """
+   Query all rows in the table
+   :param conn: the Connection object
+   :return:
+   """
+   cur = conn.cursor()
+   cur.execute(f"SELECT * FROM {table}")
+   rows = cur.fetchall()
+
+   return rows
+
+def select_where(conn, table, **query):
+   """
+   Query tasks from table with data from **query dict
+   :param conn: the Connection object
+   :param table: table name
+   :param query: dict of attributes and values
+   :return:
+   """
+   cur = conn.cursor()
+   qs = []
+   values = ()
+   for k, v in query.items():
+       qs.append(f"{k}=?")
+       values += (v,)
+   q = " AND ".join(qs)
+   cur.execute(f"SELECT * FROM {table} WHERE {q}", values)
+   rows = cur.fetchall()
+   return rows
+
+def delete_all(conn, table):
+   """
+   Delete all rows from table
+   :param conn: Connection to the SQLite database
+   :param table: table name
+   :return:
+   """
+   sql = f'DELETE FROM {table}'
+   cur = conn.cursor()
+   cur.execute(sql)
+   conn.commit()
+   print("Deleted")
+
+# MAIN -  OPISANIE PAR. OBIEKTÓW, WYWOŁANIE F.
 if __name__ == "__main__":
 
    create_projects_sql = """
@@ -48,78 +148,28 @@ if __name__ == "__main__":
    -- zadanie table
    CREATE TABLE IF NOT EXISTS tasks (
       id integer PRIMARY KEY,
-      project_id integer NOT NULL,
+      projekt_id integer NOT NULL,
       nazwa VARCHAR(250) NOT NULL,
       opis TEXT,
       status VARCHAR(15) NOT NULL,
       start_date text NOT NULL,
       end_date text NOT NULL,
-      FOREIGN KEY (project_id) REFERENCES projects(id)
+      FOREIGN KEY (projekt_id) REFERENCES projects(id)
    );
    """
-
-   db_file = "database.db"
-
+      
    conn = create_connection(db_file)
    if conn is not None:
        execute_sql(conn, create_projects_sql)
        execute_sql(conn, create_tasks_sql)
-       conn.close()
+       #conn.close()
 
-# ex_03.py
-import sqlite3
-
-def create_connection(db_file):
-   """ create a database connection to the SQLite database
-       specified by db_file
-   :param db_file: database file
-   :return: Connection object or None
-   """
-   conn = None
-   try:
-       conn = sqlite3.connect(db_file)
-       return conn
-   except sqlite3.Error as e:
-       print(e)
-   return conn
-
-def add_project(conn, project):
-   """
-   Create a new project into the projects table
-   :param conn:
-   :param project:
-   :return: project id
-   """
-   sql = '''INSERT INTO projects(nazwa, start_date, end_date)
-             VALUES(?,?,?)'''
-   cur = conn.cursor()
-   cur.execute(sql, project)
-   conn.commit()
-   return cur.lastrowid
-
-def add_task(conn, task):
-   """
-   Create a new task into the tasks table
-   :param conn:
-   :param task:
-   :return: task id
-   """
-   sql = '''INSERT INTO tasks(project_id, nazwa, opis, status, start_date, end_date)
-             VALUES(?,?,?,?,?,?)'''
-   cur = conn.cursor()
-   cur.execute(sql, task)
-   conn.commit()
-   return cur.lastrowid
-
-if __name__ == "__main__":
-   project = ("Powtórka z angielskiego", "2020-05-11 00:00:00", "2020-05-13 00:00:00")
-
-   conn = create_connection("database.db")
-   project_id = add_project(conn, project)
+   project = ("Inne powtórki", "2020-05-11 00:00:00", "2020-05-13 00:00:00")
+   pr_id = add_project(conn, project)
 
    task = (
-       project_id,
-       "Czasowniki regularne",
+       pr_id,
+       "Czasowniki nieregularne",
        "Zapamiętaj czasowniki ze strony 30",
        "started",
        "2020-05-11 12:00:00",
@@ -128,72 +178,11 @@ if __name__ == "__main__":
 
    task_id = add_task(conn, task)
 
-   print(project_id, task_id)
+   print(pr_id, task_id)
    conn.commit()
+   print(select_task_by_status(conn, status="started"))
 
-# from ex_04_selecty import *
-#import sqlite3
-#from sqlite3 import Error
+   #delete_all(conn, "tasks")
+   #delete_all(conn, "projects")
 
-#def create_connection(db_file):
-   """ create a database connection to the SQLite database
-       specified by the db_file
-   :param db_file: database file
-   :return: Connection object or None
-   """
-#   conn = None
-#   try:
-#       conn = sqlite3.connect(db_file)
-#   except Error as e:
-#       print(e)
 
-#   return conn
-
-#def select_task_by_status(conn, status):
-   """
-   Query tasks by priority
-   :param conn: the Connection object
-   :param status:
-   :return:
-   """
-#   cur = conn.cursor()
-#   cur.execute("SELECT * FROM tasks WHERE status=?", ("started", ))
-
-#   rows = cur.fetchall()
-#   return rows
-
-#def select_all(conn, table):
-   """
-   Query all rows in the table
-   :param conn: the Connection object
-   :return:
-   """
-#   cur = conn.cursor()
-#   cur.execute(f"SELECT * FROM {table}")
-#   rows = cur.fetchall()
-
-#   return rows
-
-#def select_where(conn, table, **query):
-   """
-   Query tasks from table with data from **query dict
-   :param conn: the Connection object
-   :param table: table name
-   :param query: dict of attributes and values
-   :return:
-   """
-#   cur = conn.cursor()
-#   qs = []
-#   values = ()
-#   for k, v in query.items():
-#       qs.append(f"{k}=?")
-#       values += (v,)
-#   q = " AND ".join(qs)
-#   cur.execute(f"SELECT * FROM {table} WHERE {q}", values)
-#   rows = cur.fetchall()
-#   return rows
-
-   #https://python101.readthedocs.io/pl/latest/bazy/sql/
-
-import sqlalchemy
-sqlalchemy.__version__
--- bazydanych2.py
+++ bazydanych2.py
@@ -1,5 +1,3 @@
-# `ex_01_conection_to_db.py`
-
 import sqlite3
 from sqlite3 import Error
 
@@ -11,25 +9,26 @@ def create_connection(db_file):
        print(f"Connected to {db_file}, sqlite version: {sqlite3.version}")
    except Error as e:
        print(e)
-   finally:
-       if conn:
-           conn.close()
+   return conn
+   #finally:
+   #    if conn:
+   #        conn.close()
 
-def create_connection_in_memory():
-   """ create a database connection to a SQLite database """
-   conn = None
-   try:
-       conn = sqlite3.connect(":memory:")
-       print(f"Connected, sqlite version: {sqlite3.version}")
-   except Error as e:
-       print(e)
-   finally:
-       if conn:
-           conn.close()
+#def create_connection_in_memory():
+#   """ create a database connection to a SQLite database """
+#   conn = None
+#   try:
+#       conn = sqlite3.connect(":memory:")
+#       print(f"Connected, sqlite version: {sqlite3.version}")
+#   except Error as e:
+#       print(e)
+#   finally:
+#      if conn:
+#           conn.close()
 
 if __name__ == '__main__':
    create_connection(r"database.db")
-   create_connection_in_memory()
+   #create_connection_in_memory()
 
 def execute_sql(conn, sql):
    """ Execute sql
